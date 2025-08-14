29afda173ceaa2cfb9e3391541f02ec7f02ec161
SEA <the@e.ventures>
2025-08-13 20:08:21 -0700

    added voicebot to services

--- webapp/config/services.php
+++ webapp/config/services.php
@@ -28,6 +28,10 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'voicebot' => [
+        'base_url' => env('VOICEBOT_BASE_URL', 'http://localhost:8000'),
+    ],
+
     'slack' => [
         'notifications' => [
             'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
